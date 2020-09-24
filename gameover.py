bc05f758469a941545f767308c68917230118186
Joe Chung <jchung@GSLPTAP0000089.local>
2020-09-24 09:32:18 -0700

    gameWin

--- gameover.py
+++ gameover.py
@@ -10,3 +10,10 @@ class gameOver():
         print("You are back in your home without the ingredients to make the amazing banana bread.")
         print(gameOverArt)
         sys.exit()
+
+class gameWin():
+    
+    def win(self):
+        print("You are back at home enjoying your amazing banana bread!")
+        print(gameWinArt)
+        sys.exit()
